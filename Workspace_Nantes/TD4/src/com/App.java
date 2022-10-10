d5e82052f50ad1c85046ff9249defc6787fbb24b
Axel Guerin <59256787+axelguerinoctave@users.noreply.github.com>
2022-10-10 17:50:03 +0200

    Cours 10/10

--- Workspace_Nantes/TD4/src/com/App.java
+++ Workspace_Nantes/TD4/src/com/App.java
@@ -12,6 +12,36 @@ public class App {
 		System.out.println("]");
 	}
 	
+	public static int sommeTableau(int[] tableau) {
+		int somme = 0;
+		for(int i = 0; i < tableau.length; i++) {
+			somme = somme + tableau[i];
+		}
+		return somme;
+	}
+	
+	public static int sommeTableau2(int[] tableau) {
+		int somme = 0;
+		for(int elem : tableau) {
+			somme += elem;
+		}
+		return somme;
+	}
+	
+	public static void estPair(int[] tableau) {
+		boolean pair = true;
+		for (int elem : tableau) {
+			if (elem % 2 != 0) {
+				pair = false; 
+			}
+		}
+		if (pair) {
+			System.out.println("Le tableau est pair");
+		} else {
+			System.out.println("Le tableau n'est pas pair");
+		}
+	}
+	
 	public static void main(String[] args) {
 		int n = 4;
 		double x = 5.3;
@@ -27,8 +57,11 @@ public class App {
 		for(int i = 0; i <= 9; i++) {
 			tableau[i] = 2 * (i + 1);
 		}
-		
 		afficherTableau(tableau);
+		System.out.println("La somme du tableau est : " + sommeTableau(tableau));
+		estPair(tableau);
+		tableau[5] = 3;
+		estPair(tableau);
 	}
 
 }
--- Workspace_Nantes/TD5/src/com/App.java
+++ Workspace_Nantes/TD5/src/com/App.java
@@ -1,6 +1,9 @@
 package com;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JSpinner.ListEditor;
 
 public class App {
 
@@ -8,7 +11,7 @@ public class App {
 	// Utiliser uniquement add, remove(0), isEmpty et get(0)
 	
 	public static void afficherListe(ArrayList<Integer> liste) {
-		if(!liste.isEmpty()) {
+		if(!liste.isEmpty()) { // cas d'arret
 			System.out.println(liste.get(0));
 			liste.remove(0);
 			afficherListe(liste);
@@ -17,7 +20,7 @@ public class App {
 	
 	// Fonction sommant l'ensemble des elements de la liste
 	public static int sommeListe(ArrayList<Integer> liste) {
-		if(liste.isEmpty()) {
+		if(liste.isEmpty()) { // cas d'arret
 			return 0;
 		}else {
 			int e = liste.get(0);
@@ -26,24 +29,47 @@ public class App {
 		}
 	}
 	
+	public static int sommeListe2(ArrayList<Integer> liste) {
+		return sommeListe2_aux(liste, 0);
+	}
+	
+	public static int sommeListe2_aux(ArrayList<Integer> liste, int somme) {
+		if (liste.isEmpty()) {
+			return somme;
+		}else {
+			int e = liste.get(0);
+			liste.remove(0);
+			return sommeListe2_aux(liste, e + somme);
+		}
+	}
+	
+	public static ArrayList<Integer> carreListe(ArrayList<Integer> liste){
+		return carreListe_aux(liste, new ArrayList<Integer>());
+	}
+	
+	public static ArrayList<Integer> carreListe_aux(ArrayList<Integer> liste,
+												ArrayList<Integer> liste_carre){
+		if (liste.isEmpty()) {
+			return liste_carre;
+		} else {
+			int e = liste.get(0);
+			liste.remove(0);
+			liste_carre.add(e * e);
+			return carreListe_aux(liste, liste_carre);
+		}
+	}
+	
+	public static ArrayList<Integer> sommeVecteur(ArrayList<Integer> u, 
+												  ArrayList<Integer> v){
+		if(u.isEmpty() || v.isEmpty()) {
+			
+		}
+	}
+	
 	public static void main(String[] args) {
 		
-		ArrayList<Integer> liste = new ArrayList<Integer>();
-		
-		for(int i = 1; i <= 10; i++) {
-			liste.add(i);
-		}
-		
-		//afficherListe(liste);
-		System.out.println(sommeListe(liste));
-		
-		
-		/*
-		System.out.println(liste.isEmpty());
-		
-		System.out.println(liste.get(6)); // equivalent tableau[6]
-		liste.remove(0);
-		System.out.println(liste);*/
+		ArrayList<Integer> liste = new ArrayList<Integer>(List.of(1,2,3));
+		System.out.println(sommeListe2(liste));
 	}
 
 }
