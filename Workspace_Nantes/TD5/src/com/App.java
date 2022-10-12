58e6715118e0503e8479f63075323e3b92734a4a
Axel Guerin <59256787+axelguerinoctave@users.noreply.github.com>
2022-10-12 14:52:04 +0200

    cours 12/10

--- Workspace_Nantes/TD5/src/com/App.java
+++ Workspace_Nantes/TD5/src/com/App.java
@@ -7,9 +7,16 @@ import javax.swing.JSpinner.ListEditor;
 
 public class App {
 
-	
+	public static ArrayList<Integer> newList(int[] tableau) {
+		ArrayList<Integer> liste = new ArrayList<Integer>();
+		for(int e : tableau) {
+			liste.add(e);
+		}
+		return liste;
+	}		
+
 	// Utiliser uniquement add, remove(0), isEmpty et get(0)
-	
+
 	public static void afficherListe(ArrayList<Integer> liste) {
 		if(!liste.isEmpty()) { // cas d'arret
 			System.out.println(liste.get(0));
@@ -17,7 +24,7 @@ public class App {
 			afficherListe(liste);
 		}
 	}
-	
+
 	// Fonction sommant l'ensemble des elements de la liste
 	public static int sommeListe(ArrayList<Integer> liste) {
 		if(liste.isEmpty()) { // cas d'arret
@@ -28,11 +35,11 @@ public class App {
 			return e + sommeListe(liste);
 		}
 	}
-	
+
 	public static int sommeListe2(ArrayList<Integer> liste) {
 		return sommeListe2_aux(liste, 0);
 	}
-	
+
 	public static int sommeListe2_aux(ArrayList<Integer> liste, int somme) {
 		if (liste.isEmpty()) {
 			return somme;
@@ -42,13 +49,13 @@ public class App {
 			return sommeListe2_aux(liste, e + somme);
 		}
 	}
-	
+
 	public static ArrayList<Integer> carreListe(ArrayList<Integer> liste){
 		return carreListe_aux(liste, new ArrayList<Integer>());
 	}
-	
+
 	public static ArrayList<Integer> carreListe_aux(ArrayList<Integer> liste,
-												ArrayList<Integer> liste_carre){
+			ArrayList<Integer> liste_carre){
 		if (liste.isEmpty()) {
 			return liste_carre;
 		} else {
@@ -58,18 +65,46 @@ public class App {
 			return carreListe_aux(liste, liste_carre);
 		}
 	}
-	
-	public static ArrayList<Integer> sommeVecteur(ArrayList<Integer> u, 
-												  ArrayList<Integer> v){
-		if(u.isEmpty() || v.isEmpty()) {
-			
+
+	public static ArrayList<Integer> concatenation(ArrayList<Integer> liste_a,
+			ArrayList<Integer> liste_b){
+		if (liste_b.isEmpty()) {
+			return liste_a;
+		}else {
+			int e = liste_b.get(0);
+			liste_b.remove(0);
+			liste_a.add(e);
+			return concatenation(liste_a, liste_b);
 		}
 	}
-	
+
+	public static ArrayList<Integer> sommeVecteur(ArrayList<Integer> u,
+			ArrayList<Integer> v){
+		return sommeVecteur_aux(u, v, new ArrayList<Integer>());
+	}
+
+	public static ArrayList<Integer> sommeVecteur_aux(ArrayList<Integer> u,
+			ArrayList<Integer> v,
+			ArrayList<Integer> res){
+		if (u.isEmpty() || v.isEmpty()) {
+			return res;
+		} else {
+			int e_u = u.get(0);
+			int e_v = v.get(0);
+			u.remove(0);
+			v.remove(0);
+			res.add(e_u + e_v);
+			return sommeVecteur_aux(u, v, res);
+		}
+	}
+
 	public static void main(String[] args) {
-		
-		ArrayList<Integer> liste = new ArrayList<Integer>(List.of(1,2,3));
-		System.out.println(sommeListe2(liste));
+		int[] tab_a = {2, 5, 3};
+		ArrayList<Integer> liste_a = newList(tab_a);
+		int[] tab_b = {1, 3, 7};
+		ArrayList<Integer> liste_b = newList(tab_b);
+		System.out.println(sommeVecteur(liste_a, liste_b));
+		System.out.println(sommeVecteur(liste_a, liste_b));
 	}
 
 }
