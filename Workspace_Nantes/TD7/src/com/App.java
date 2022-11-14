a38140023e57fce55bf3ded93a5321f0404892fe
Axel Guerin <59256787+axelguerinoctave@users.noreply.github.com>
2022-11-14 09:41:57 +0100

    Add files via upload

--- Workspace_Nantes/TD7/src/com/App.java
+++ Workspace_Nantes/TD7/src/com/App.java
@@ -1,5 +1,7 @@
 package com;
 
+import java.util.ArrayList;
+
 public class App {
 
 	public static void parcours1(Noeud arbre) {
@@ -36,9 +38,48 @@ public class App {
 		if(arbre.isEmpty()) {
 			return Integer.valueOf(arbre.valeur);
 		}else {
-			calcul(arbre.gauche)
-			calcul(arbre.droit)
+			if(arbre.valeur.equals("+")) {
+				return calcul(arbre.gauche) + calcul(arbre.droit);
+			}else if(arbre.valeur.equals("*")) {
+				return calcul(arbre.gauche) * calcul(arbre.droit);
+			}else if(arbre.valeur.equals("-")) {
+				return calcul(arbre.gauche) - calcul(arbre.droit);
+			}else{
+				return calcul(arbre.gauche) / calcul(arbre.droit);
+			}
 		}
+	}
+	
+	public static ArrayList<String> stringToList(String exp){
+		return stringToList_aux(exp, new ArrayList<String>(), "");
+	}
+	
+	public static ArrayList<String> stringToList_aux(String exp, 
+													 ArrayList<String> liste,
+													 String nombre_stock){
+		if(exp.equals("")) {
+			liste.add(nombre_stock);
+			return liste;
+		}else {
+			String e = exp.substring(0, 1); //get(0)
+			exp = exp.substring(1); //remove(0);
+			if(e.equals(" ")) {
+				liste.add(nombre_stock);
+				nombre_stock = "";
+			}else {
+				nombre_stock += e;
+			}
+			return stringToList_aux(exp, liste, nombre_stock);
+		}
+	}
+	
+	public static Noeud remplirArbre(String exp) {
+		ArrayList<String> liste = stringToList(exp);
+		return remplirArbre_aux(liste);
+	}
+	
+	public static Noeud remplirArbre_aux(ArrayList<String> liste) {
+		return null;
 	}
 	
 	public static void main(String[] args) {
@@ -47,7 +88,7 @@ public class App {
 										  new Noeud("3"),
 										  new Noeud("4")),
 								new Noeud("5"));
-		parcours1(arbre);
+		System.out.println(calcul(remplirArbre("* + 32 4 5")));
 	}
 
 }
